f30d7d937f086940bcf431252adfcbf293258b73
Wetzler Alexandre <alexandre_wet@hotmail.com>
2020-03-23 14:48:16 +0100

    Updates

---
+++ precache-manifest.759fe6670e0b7503d7ffd40c042fe77e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da132a52de40f0c8c17c23d5a29926af",
+    "url": "/SplitFlapMariage/index.html"
+  },
+  {
+    "revision": "90a5d25c337fd5ef850e",
+    "url": "/SplitFlapMariage/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "abc4fc6cf110f15398ad",
+    "url": "/SplitFlapMariage/static/js/2.a7d79f77.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SplitFlapMariage/static/js/2.a7d79f77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90a5d25c337fd5ef850e",
+    "url": "/SplitFlapMariage/static/js/main.873d8f28.chunk.js"
+  },
+  {
+    "revision": "bd4ce6547b18b01b5db7",
+    "url": "/SplitFlapMariage/static/js/runtime-main.d5f5d417.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/SplitFlapMariage/static/media/logo.5d5d9eef.svg"
+  }
+]);
